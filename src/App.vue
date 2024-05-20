94f7ef5897634ec3858f50bc0365833e0d21dcb2
Yorktown <example@mail.com>
2024-05-20 11:36:54 +0700

    Clear project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <ComparsionTable />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import ComparsionTable from "./components/ComparsionTable.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    ComparsionTable,
   },
 });
 </script>
---
+++ src/components/ComparsionTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ComparsionTable",
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
